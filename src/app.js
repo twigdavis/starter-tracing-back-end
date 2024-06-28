160e5515c605aa430a73679120341727faf2a2a8
Justin Davis <judavis@tesla.com>
2024-06-28 13:56:33 -0400

    update tracing logs

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const cors = require("cors");
 const articlesRouter = require("./articles/articles.router");
 const errorHandler = require("./errors/errorHandler");
 const notFound = require("./errors/notFound");
-const pinoHttp = require('pino-http');
+const logger = require('./config/logger');
 
 const app = express();
 
-app.use(pinoHttp());
+app.use(logger);
 app.use(cors());
 app.use(express.json());
 
